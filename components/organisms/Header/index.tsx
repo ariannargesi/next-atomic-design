fad69e1f903d7d65443909c9a0ba94f771635b42
Arian Nargesi <ariannargesi@gmail.com>
2020-11-12 01:24:29 +0330

    recreate

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -1,22 +1,39 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import { Wrapper, SearchBarWrapper, PhoneNumberWrapper, LogoWrapper, GroupsWrapper } from './styles'
+import Avatar from '../../atoms/Avatar'
+import Icon from '../../atoms/Icon'
+import SearchBar from '../../molecules/SearchBar'
+import { Img  } from '../../atoms/Image' 
+import { Span } from '../../atoms/Text'
 
-import { Wrapper } from './styles'
-import { Button, Heading } from '../../atoms'
+// Logo
 
 const Header: FC = () => {
+
+	const [searchBar, setSearchBar] = useState(false)
+	
+	const submitHandler = (query: string): void => {
+
+	}
+
 	return (
 		<Wrapper>
-			<Heading.H4 fontWeight={600}>Welcome to Dashboard</Heading.H4>
-			<Button
-				fontFamily={'body'}
-				fontWeight={500}
-				fontSize={12}
-				color={'white'}
-				backgroundColor={'red-400'}
-				padding={2}
-			>
-				GENERATE REPORT
-			</Button>
+			<PhoneNumberWrapper>
+				<Icon icon='phone'/>
+				<Span>98-936806-9820</Span>
+			</PhoneNumberWrapper>
+			<LogoWrapper>
+ 				<Img src={'/img/logo.png'} width={200} height={50} layout={'fixed'} />
+			 </LogoWrapper>
+			<GroupsWrapper>
+					<div style={{ position: 'relative' }}>
+						<Icon icon='search' size='lg' onClick={() => setSearchBar(!searchBar)} pointer />
+						<SearchBarWrapper>{searchBar && <SearchBar onSubmit={submitHandler}/>}</SearchBarWrapper>
+					</div>
+					<Icon icon='shopping-basket' size='lg' counter={'1'} pointer />
+					<Avatar width={40} height={40} src='https://picsum.photos/42' rounded />
+			</GroupsWrapper>
+ 
 		</Wrapper>
 	)
 }
