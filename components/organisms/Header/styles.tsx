47c26ef9b166f569c9f6032a5f124e95ec4cc016
Arian Nargesi <ariannargesi@gmail.com>
2020-11-12 01:24:46 +0330

    add wrappers

--- components/organisms/Header/styles.tsx
+++ components/organisms/Header/styles.tsx
@@ -1,7 +1,30 @@
 import styled from 'styled-components'
-
 export const Wrapper = styled.div`
 	display: flex;
+	align-items: center;
 	justify-content: space-between;
 	width: 100%;
+`
+export const SearchBarWrapper = styled.div`
+	width: 300px;
+	position: absolute; 
+	right: 100%;
+
+`
+
+export const PhoneNumberWrapper = styled.div`
+	width: 140px;
+	display: flex;
+	justify-content: space-between;
+`
+
+export const LogoWrapper = styled.div`
+
+`
+
+export const GroupsWrapper = styled.div`
+	width: 180px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `
