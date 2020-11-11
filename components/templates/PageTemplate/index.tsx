684dab3001a9a7522672c73ab76bc8f6a362a5c2
Arian Nargesi <ariannargesi@gmail.com>
2020-11-12 01:26:32 +0330

    remove old stuff

--- components/templates/PageTemplate/index.tsx
+++ components/templates/PageTemplate/index.tsx
@@ -10,9 +10,8 @@ interface PageTemplateProps {
 const PageTemplate: FC<PageTemplateProps> = ({ navigation, header, footer, children }) => {
 	return (
 		<Wrapper>
-			<Navigation>{navigation}</Navigation>
 			<Header maxWidth={['100%', '100%', 720, 960, 1140]}>{header}</Header>
-			<Content maxWidth={['100%', '100%', 720, 960, 1140]}>{children}</Content>
+			<Navigation>{navigation}</Navigation>		
 			<Footer>{footer}</Footer>
 		</Wrapper>
 	)
