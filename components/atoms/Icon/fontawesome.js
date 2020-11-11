6562ed7a28614cd74a7c2c2eca7b854bf39d2fcb
Arian Nargesi <ariannargesi@gmail.com>
2020-11-12 01:21:51 +0330

    fontawsome lib

---
+++ components/atoms/Icon/fontawesome.js
@@ -0,0 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faCode, faHighlighter, faSearch, faShoppingBasket, faPhone } from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+    faPhone,
+    faShoppingBasket,
+	faCode,
+    faHighlighter,
+    faSearch
+	// more icons go here
+)
