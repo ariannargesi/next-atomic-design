05d6a7b5248e2ea62e4987d8c08bfb72612e7a0d
Arian Nargesi <ariannargesi@gmail.com>
2020-11-12 21:03:56 +0330

    update props

--- components/atoms/Image/index.tsx
+++ components/atoms/Image/index.tsx
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
+import { border, BorderProps } from 'styled-system'
+import { themeGet } from '@styled-system/theme-get';
 import Image from 'next/image';
 
+interface ImgProps {
+    layout: string,
+    rounded?: boolean,
+    bordered?: boolean 
+}
 
-export const Img = styled(Image)<{ layout: string }>``;
+export const Img = styled(Image)<ImgProps & BorderProps>`
+    ${props => props.rounded && 'border-radius: 100%'};
+    ${props => props.bordered && 'border: ' + '2px solid lightblue !important' };
+    ${border}
+
+`
