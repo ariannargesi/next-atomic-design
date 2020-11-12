f461cd6b9d0a4ada515bebf73f42b1d6dd2b814c
Arian Nargesi <ariannargesi@gmail.com>
2020-11-12 21:08:08 +0330

    initial

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -1,39 +1,34 @@
 import { FC, useState } from 'react'
 import { Wrapper, SearchBarWrapper, PhoneNumberWrapper, LogoWrapper, GroupsWrapper } from './styles'
-import Avatar from '../../atoms/Avatar'
 import Icon from '../../atoms/Icon'
 import SearchBar from '../../molecules/SearchBar'
-import { Img  } from '../../atoms/Image' 
+import { Img } from '../../atoms/Image'
 import { Span } from '../../atoms/Text'
 
 // Logo
 
 const Header: FC = () => {
-
 	const [searchBar, setSearchBar] = useState(false)
-	
-	const submitHandler = (query: string): void => {
 
-	}
+	const submitHandler = (query: string): void => {}
 
 	return (
 		<Wrapper>
 			<PhoneNumberWrapper>
-				<Icon icon='phone'/>
-				<Span>98-936806-9820</Span>
+				<Icon icon='phone' />
+				<Span color='blue-600'>98-936806-9820</Span>
 			</PhoneNumberWrapper>
 			<LogoWrapper>
- 				<Img src={'/img/logo.png'} width={200} height={50} layout={'fixed'} />
-			 </LogoWrapper>
+				<Img src={'/img/logo.png'} width={200} height={50} layout={'fixed'} />
+			</LogoWrapper>
 			<GroupsWrapper>
-					<div style={{ position: 'relative' }}>
-						<Icon icon='search' size='lg' onClick={() => setSearchBar(!searchBar)} pointer />
-						<SearchBarWrapper>{searchBar && <SearchBar onSubmit={submitHandler}/>}</SearchBarWrapper>
-					</div>
-					<Icon icon='shopping-basket' size='lg' counter={'1'} pointer />
-					<Avatar width={40} height={40} src='https://picsum.photos/42' rounded />
+				<div style={{ position: 'relative' }}>
+					<Icon icon='search' size='lg' onClick={() => setSearchBar(!searchBar)} pointer />
+					<SearchBarWrapper>{searchBar && <SearchBar onSubmit={submitHandler} />}</SearchBarWrapper>
+				</div>
+				<Icon icon='shopping-basket' size='lg' counter={'1'} pointer />
+				<Img src={'/img/avatar.png'} width={34} height={34} layout={'fixed'} rounded bordered/>
 			</GroupsWrapper>
- 
 		</Wrapper>
 	)
 }
