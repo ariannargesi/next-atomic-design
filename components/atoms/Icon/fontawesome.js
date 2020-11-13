e1a9e60d3946994b8f8fb0a676ed00c446046ccd
Arian Nargesi <ariannargesi@gmail.com>
2020-11-13 23:25:26 +0330

    add faStar

--- components/atoms/Icon/fontawesome.js
+++ components/atoms/Icon/fontawesome.js
@@ -1,8 +1,9 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 
-import { faCode, faHighlighter, faSearch, faShoppingBasket, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faCode, faHighlighter, faSearch, faShoppingBasket, faPhone, faStar } from '@fortawesome/free-solid-svg-icons'
 
 library.add(
+    faStar,
     faPhone,
     faShoppingBasket,
 	faCode,
