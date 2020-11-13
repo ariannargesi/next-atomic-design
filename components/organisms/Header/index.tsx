71c054d1fdad6bc6c10ba445043c2b23136112b2
Arian Nargesi <ariannargesi@gmail.com>
2020-11-13 23:27:35 +0330

    add colors

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -1,36 +1,43 @@
-import { FC, useState } from 'react'
+import { FC, useState, useEffect } from 'react'
+import { themeGet } from '@styled-system/theme-get'
+import { withTheme } from 'styled-components'
 import { Wrapper, SearchBarWrapper, PhoneNumberWrapper, LogoWrapper, GroupsWrapper } from './styles'
 import Icon from '../../atoms/Icon'
 import SearchBar from '../../molecules/SearchBar'
 import { Img } from '../../atoms/Image'
 import { Span } from '../../atoms/Text'
-
 // Logo
 
-const Header: FC = () => {
+let colorNormal 
+
+const Header: FC = (props) => {
 	const [searchBar, setSearchBar] = useState(false)
 
 	const submitHandler = (query: string): void => {}
 
+	const getColor = (color: string) => {
+		return themeGet(`colors.${color}`)(props)
+	}
+
 	return (
 		<Wrapper>
 			<PhoneNumberWrapper>
-				<Icon icon='phone' />
-				<Span color='blue-600'>98-936806-9820</Span>
+				<Icon icon='phone' color={getColor('normal')} />
+				<Span color='normal'>98-936806-9820</Span>
 			</PhoneNumberWrapper>
 			<LogoWrapper>
 				<Img src={'/img/logo.png'} width={200} height={50} layout={'fixed'} />
 			</LogoWrapper>
 			<GroupsWrapper>
 				<div style={{ position: 'relative' }}>
-					<Icon icon='search' size='lg' onClick={() => setSearchBar(!searchBar)} pointer />
+					<Icon icon='search' size='lg' onClick={() => setSearchBar(!searchBar)} color={getColor('normal')} pointer  />
 					<SearchBarWrapper>{searchBar && <SearchBar onSubmit={submitHandler} />}</SearchBarWrapper>
 				</div>
-				<Icon icon='shopping-basket' size='lg' counter={'1'} pointer />
+				<Icon icon='shopping-basket' size='lg' counter={'1'} pointer color={getColor('normal') } />
 				<Img src={'/img/avatar.png'} width={34} height={34} layout={'fixed'} rounded bordered/>
 			</GroupsWrapper>
 		</Wrapper>
 	)
 }
-
-export default Header
+// @ts-ignore
+export default withTheme(Header)
